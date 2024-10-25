05496be16100e3086ee139c79dc877c413b0d54e
Kangdy <canfrog@naver.com>
2024-10-26 00:26:05 +0900

    ✨ [Feat] 세부 페이지 연결

--- src/component/CardLayout.jsx
+++ src/component/CardLayout.jsx
@@ -1,31 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function CardLayout(props) {
+    const navigate = useNavigate();
     const url = '/img/player/';
     return (
         <div className="background">
                 <div className="card_layout">
                     {
-                            props.seasonInfo[props.order].map((a, i)=>{
-                                return (
-                                    <div className="card" key={i}>
-                                        <span className="card__number">{a.backnumber}</span>
-                                        <a href="/">
-                                            <div className="card__name">
-                                            <span>{a.first_name}</span>
-                                                <h3>{a.last_name}</h3>
-                                            </div>
-                                            <div className="card__rectangle">
-                                                <img src={url + '/' + a.season + '/' + a.season + a.last_name + '.webp'} alt=""/>
-                                            </div>
-                                            <p className="card__detail">
-                                                <span>{a.position}</span> 
-                                            </p>
-                                        </a>
-                                    </div>  
-                                )
-                            }) 
-                            
-                    // })
-                }
+                        props.seasonInfo[props.order].map((a, i)=>{
+                            const handleClick = ()=>{
+                                navigate(`/player/${a.season}/${a.backnumber}`)
+                            }
+
+                            return (
+                                <div className="card" key={i} onClick={()=>handleClick()}>
+                                    <span className="card__number">{a.backnumber}</span>
+                                    <div className="card__name">
+                                    <span>{a.first_name}</span>
+                                        <h3>{a.last_name}</h3>
+                                    </div>
+                                    <div className="card__rectangle">
+                                        <img src={url + '/' + a.season + '/' + a.season + a.last_name + '.webp'} alt=""/>
+                                    </div>
+                                    <p className="card__detail">
+                                        <span>{a.position}</span> 
+                                    </p>
+                                </div>  
+                            )
+                        }) 
+                    }
             </div>
         </div>
     )
