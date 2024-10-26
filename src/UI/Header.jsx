cd1099a69c25aeee3f1ff1137ab79c573038a02d
Kangdy <canfrog@naver.com>
2024-10-27 00:19:06 +0900

    Add 2425 Player Img && Make Readme.md more specific

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -3,6 +3,7 @@ export default function Header() {
         <header className="header">
             <img src="/img/Logo.webp" alt=""/>
             <h1>Chelsea FC Player Data</h1>
+            <p>Pride of London</p>
         </header>
     )
 }
