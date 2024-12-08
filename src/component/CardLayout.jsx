40ab5acf914781c34a7bf04d859a4e79f4720ca8
Kangdy <canfrog@naver.com>
2024-12-08 23:23:41 +0900

    Apply Tailwind in Card Component

--- src/component/CardLayout.jsx
+++ src/component/CardLayout.jsx
@@ -4,8 +4,8 @@ export default function CardLayout(props) {
     const navigate = useNavigate();
     const url = '/img/player/';
     return (
-        <div className="background">
-                <div className="card_layout">
+        <div className="background absolute left-[200px] w-calc-full-200px bg-background-gradient md:left-0 md:w-full md:top-[175px] sm:top-[330px]">
+                <div className="card_layout grid grid-cols-[repeat(auto-fill,minmax(250px,0.5fr))] my-5 ml-5 justify-center content-center gap-2.5 px-5 pb-5">
                     {
                         props.seasonInfo[props.order].map((a, i)=>{
                             // url에 맞게 이름 변환
@@ -21,17 +21,21 @@ export default function CardLayout(props) {
                             }
 
                             return (
-                                <div className="card" key={i} onClick={()=>handleClick()}>
-                                    <span className="card__number">{a.backnumber}</span>
-                                    <div className="card__name">
-                                    <span>{a.first_name}</span>
-                                        <h3>{a.last_name}</h3>
+                                <div className="card flex justify-center items-center flex-col w-[250px] h-[450px] mt-5 border border-slate-300 rounded-2xl bg-card-gradient text-center backdrop-blur-[10px] shadow-cardShadow 
+                                hover:shadow-cardHoverShadow hover:transform hover:scale-105 hover:transition hover:duration-1000" 
+                                
+                                key={i} onClick={()=>handleClick()}>
+                                    <span className="card__number text-white opacity-50 text-[40px] pt-[10px] pl-4 font-serif absolute top-0 left-0 z-[2] tracking-[-3px]">{a.backnumber}</span>
+                                    <div className="card__name flex flex-col justify-center items-stretch font-serif text-white font-semibold">
+                                    <span className='text-[16px] text-slate-200 mt-1'>{a.first_name}</span>
+                                        <h3 className='text-[26px] text-center mt-2'>{a.last_name}</h3>
                                     </div>
-                                    <div className="card__rectangle">
-                                        <img src={url + '/' + a.season + '/' + a.season + a.last_name + '.webp'} alt=""/>
+                                    <div className="card__rectangle bg-customCobalt border border-slate-500 backdrop-blur-[40px] shadow-cardShadow rounded-[20px] w-[200px] h-[250px] overflow-hidden">
+                                        <img className='absolute top-0 left-0 w-full'
+                                        src={url + '/' + a.season + '/' + a.season + a.last_name + '.webp'} alt=""/>
                                     </div>
-                                    <p className="card__detail">
-                                        <span>{a.position}</span> 
+                                    <p className="card__detail text-white p-4 text-[13px]">
+                                        <span className='text-[15px] font-serif font-bold'>{a.position}</span> 
                                     </p>
                                 </div>  
                             )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,14 @@ module.exports = {
       extend: {
         backgroundImage: {
           'radial-eclipse-bottom' : 'radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)',
-          'cyan-gradient' : 'linear-gradient(45deg, rgb(103, 137, 250), rgb(47, 50, 234))'
-
+          'cyan-gradient' : 'linear-gradient(45deg, rgb(103, 137, 250), rgb(47, 50, 234))',
+          'background-gradient' : 'radial-gradient(ellipse at bottom, #262d6a, #3253d6, #07002e)',
+          'card-gradient' : 'linear-gradient(45deg, #0e0542, #172286)'
         },
         colors : {
           customSkyblue: '#b8bde3',
-          customBlue: 'rgb(36, 57, 149)'
+          customBlue: 'rgb(36, 57, 149)',
+          customCobalt: 'rgba(104, 116, 189, 0.3)'
         },
         screens: {
           sm: {  max: "480px" },
@@ -19,10 +21,13 @@ module.exports = {
         },
         fontFamily: {
           sans: ['"Noto Sans KR"', 'sans-serif'],
+          serif: ['serif']
         },
         boxShadow: {
           dropboxShadow: '3px 3px 3px 5px rgba(63, 63, 156, 0.04)',
-          sortbtnHoverShadow: 'rgba(0, 255, 255, 0.525) 0px 5px 20px;'
+          sortbtnHoverShadow: 'rgba(0, 255, 255, 0.525) 0px 5px 20px;',
+          cardShadow: '0 10px 25px rgba(51, 51, 51, 0.8)',
+          cardHoverShadow: '0px 10px 20px rgba(4, 0, 255)'
         },
       },
   },
