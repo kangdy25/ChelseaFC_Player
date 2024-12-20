d2935e40c2420c2ef74c586b4fe2e6fe55132ad9
Kangdy <canfrog@naver.com>
2024-12-20 14:47:11 +0900

    ✨ [Feat] 스탯 세부 페이지 구현 (2)

--- src/component/FieldPlayer.jsx
+++ src/component/FieldPlayer.jsx
@@ -3,17 +3,61 @@ import { useSelector } from 'react-redux';
 
 const FieldPlayer = () => {
     const stats = useSelector((state) => state.stats)
-    
+    console.log(stats)
   return (
-    <div className='block w-full h-screen bg-black bg-stat-gradient text-white'>
-        <div>
-          <div className='flex md:flex-col'>
-            {/* 선수 프로필 */}
-            <div className='flex-1 h-56 border border-blue-300 rounded-2xl ml-2 my-2'>
-              <img className='h-full float-left md:h-20' src={`/img/player/${stats.season}/${stats.season}${stats.last_name}.webp`} alt="" />
-                <h4 className='float-start text-lg mt-5'>{`${stats.season} - ${stats.first_name} ${stats.last_name}`}</h4>
+    <div className='block w-full h-full min-h-screen bg-black bg-stat-gradient text-white'>
+        <div className=''>
+          {/* 선수 프로필 */}
+          <div className='flex justify-around h-80 border border-blue-300 rounded-2xl ml-2 my-2 overflow-hidden md:flex-col md:h-full'>
+            <div className='flex flex-row gap-10 md:h-80 md:overflow-hidden md:justify-center md:pt-6'>
+              <div className='relative'>
+                <div className='z-0 absolute bottom-[-50%] w-full h-full rounded-full bg-slate-800'>.</div>
+                <img className='relative w-full h-full max-h-96 p-5 pb-0' src={`/img/player/${stats.season}/${stats.season}${stats.last_name}.webp`} alt="" />
+              </div>
+                <div className='flex flex-col justify-around ml-5 my-2'>
+                  <p className=' text-slate-500 text-xl'>{stats.season} Season</p>
+                  <div>
+                    <h5 className='font-semibold text-4xl text-slate-300'>{stats.first_name}</h5>
+                    <h4 className='font-bold text-6xl'>{stats.last_name}</h4>
+                  </div>
+                  <p className='text-8xl font-normal font-serif text-center text-slate-500'>{`${stats.backnumber}`}</p>
+                </div>
             </div>
-            <div className='flex-1 h-56 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl border border-slate-500  rounded-2xl ml-2 my-2'>
+            
+            <div className='flex flex-col justify-center gap-7 flex-wrap rounded-2xl md:my-8'>
+                <div className='flex flex-row md:justify-center md:flex-wrap md:gap-3'>
+                  <div className='w-52 mx-2'>
+                    <p className='text-slate-400 font-semibold text-sm'>Nationality</p> 
+                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.nationality}</div>
+                  </div>
+                  <div className='w-52 mx-2'>
+                    <p className='text-slate-400 font-semibold text-sm'>Place of Birth</p> 
+                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.placeOfBirth}</div>
+                  </div>
+                  <div className='w-52 mx-2'>
+                    <p className='text-slate-400 font-semibold text-sm'>Position</p> 
+                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.position}</div>
+                  </div>
+                </div>
+                <div className='flex flex-row md:justify-center md:flex-wrap md:gap-3'>
+                  <div className='w-52 mx-2'>
+                    <p className='text-slate-400 font-semibold text-sm'>Date of Birth</p> 
+                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.dob}</div>
+                  </div>
+                  <div className='w-52 mx-2'>
+                    <p className='text-slate-400 font-semibold text-sm'>Height</p> 
+                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.height}</div>
+                  </div>
+                  <div className='w-52 mx-2'>
+                    <p className='text-slate-400 font-semibold text-sm'>Debut</p> 
+                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.debut}</div>
+                  </div>
+                </div>
+                
+            </div>
+          </div>
+          <div className='flex sm:flex-col'>
+            <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl ml-2 my-2'>
               <ul>
                 {/* 출전 데이터 */}
                   <li>Appearances: {stats.appearances}</li>
@@ -22,7 +66,7 @@ const FieldPlayer = () => {
                   <li>Subbed On/Off: {stats.subbedOnOff}</li>
               </ul>
             </div>
-            <div className='flex-1 h-56 border border-blue-300 rounded-2xl m-2'>
+            <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl ml-2 my-2'>
               <ul>
                 {/* 볼 터치 데이터 */}
                   <li>Total Touches: {stats.totalTouches}</li>
@@ -34,7 +78,7 @@ const FieldPlayer = () => {
               </ul>
             </div>
           </div>
-          <div className='flex md:flex-col'>
+          <div className='flex sm:flex-col'>
             <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl ml-2 my-2'>
               <ul>
                 {/* 득점 데이터 */}
