df0779aa5104a187afbf4a64213797ad47a7dc50
Shineast <canfrog@naver.com>
2024-12-20 17:23:26 +0900

    Styling StatDetail Page

--- src/component/FieldPlayer.jsx
+++ src/component/FieldPlayer.jsx
@@ -3,12 +3,31 @@ import { useSelector } from 'react-redux';
 
 const FieldPlayer = () => {
     const stats = useSelector((state) => state.stats)
-    console.log(stats)
+  const playerProfile = [
+    { label: 'Nationality', value: stats.nationality },
+    { label: 'Place of Birth', value: stats.placeOfBirth },
+    { label: 'Position', value: stats.position },
+    { label: 'Date of Birth', value: stats.dob },
+    { label: 'Height', value: stats.height },
+    { label: 'Debut', value: stats.debut },
+  ]
+
+    const playerProfile1 = [
+      { label: 'Nationality', value: stats.nationality },
+      { label: 'Place of Birth', value: stats.placeOfBirth },
+      { label: 'Position', value: stats.position },
+    ];
+    const playerProfile2 = [
+      { label: 'Date of Birth', value: stats.dob },
+      { label: 'Height', value: stats.height },
+      { label: 'Debut', value: stats.debut },
+    ];
+
   return (
-    <div className='block w-full h-full min-h-screen bg-black bg-stat-gradient text-white'>
+    <div className='w-full h-full min-h-screen bg-black bg-stat-gradient text-white '>
         <div className=''>
           {/* 선수 프로필 */}
-          <div className='flex justify-around h-80 border border-blue-300 rounded-2xl ml-2 my-2 overflow-hidden md:flex-col md:h-full'>
+          <div className='flex justify-around h-96 border border-blue-300 rounded-2xl mx-2 my-2 overflow-hidden md:flex-col md:h-full'>
             <div className='flex flex-row gap-10 md:h-80 md:overflow-hidden md:justify-center md:pt-6'>
               <div className='relative'>
                 <div className='z-0 absolute bottom-[-50%] w-full h-full rounded-full bg-slate-800'>.</div>
@@ -24,39 +43,21 @@ const FieldPlayer = () => {
                 </div>
             </div>
             
-            <div className='flex flex-col justify-center gap-7 flex-wrap rounded-2xl md:my-8'>
-                <div className='flex flex-row md:justify-center md:flex-wrap md:gap-3'>
-                  <div className='w-52 mx-2'>
-                    <p className='text-slate-400 font-semibold text-sm'>Nationality</p> 
-                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.nationality}</div>
-                  </div>
-                  <div className='w-52 mx-2'>
-                    <p className='text-slate-400 font-semibold text-sm'>Place of Birth</p> 
-                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.placeOfBirth}</div>
-                  </div>
-                  <div className='w-52 mx-2'>
-                    <p className='text-slate-400 font-semibold text-sm'>Position</p> 
-                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.position}</div>
-                  </div>
-                </div>
-                <div className='flex flex-row md:justify-center md:flex-wrap md:gap-3'>
-                  <div className='w-52 mx-2'>
-                    <p className='text-slate-400 font-semibold text-sm'>Date of Birth</p> 
-                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.dob}</div>
-                  </div>
-                  <div className='w-52 mx-2'>
-                    <p className='text-slate-400 font-semibold text-sm'>Height</p> 
-                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.height}</div>
-                  </div>
-                  <div className='w-52 mx-2'>
-                    <p className='text-slate-400 font-semibold text-sm'>Debut</p> 
-                    <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>{stats.debut}</div>
-                  </div>
-                </div>
-                
+            <div className='flex flex-col justify-center gap-7 flex-wrap rounded-2xl md:flex-row md:py-8'>
+              {/* 반복되는 부분 map 메서드 활용하여 간결하게 처리 */}
+                    {
+                      playerProfile.map((stat, index) => (
+                        <div key={index} className='w-64 mx-2'>
+                          <p className='text-slate-400 font-semibold text-sm'>{stat.label}</p>
+                          <div className='flex items-center justify-center border border-blue-300 rounded-md h-14 font-serif font-bold mt-2.5'>
+                            {stat.value}
+                          </div>
+                        </div>
+                      ))
+                    }
             </div>
           </div>
-          <div className='flex sm:flex-col'>
+          <div className='flex h-full sm:flex-col'>
             <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl ml-2 my-2'>
               <ul>
                 {/* 출전 데이터 */}
@@ -77,8 +78,15 @@ const FieldPlayer = () => {
                   <li>Blocks: {stats.Blocks}</li>
               </ul>
             </div>
+            <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl m-2'>
+              <ul>
+                {/* 파울 데이터 */}
+                  <li>Fouls Drawn: {stats.FoulsDrawn}</li>
+                  <li>Fouls Committed: {stats.FoulsCommitted}</li>
+              </ul>
+            </div>
           </div>
-          <div className='flex sm:flex-col'>
+          <div className='flex h-full flex-wrap sm:flex-col'>
             <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl ml-2 my-2'>
               <ul>
                 {/* 득점 데이터 */}
@@ -94,13 +102,6 @@ const FieldPlayer = () => {
                   <li>Key Passes: {stats.KeyPasses}</li>
                   <li>Successful Crosses: {stats.SuccessfulCrosses}</li>
                   <li>Assists: {stats.Assists}</li>
-              </ul>
-            </div>
-            <div className='flex-1 h-56 border border-blue-300 bg-[rgba(17, 25, 40, 0.75)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-saturate-180 backdrop-blur-2xl rounded-2xl m-2'>
-              <ul>
-                {/* 파울 데이터 */}
-                  <li>Fouls Drawn: {stats.FoulsDrawn}</li>
-                  <li>Fouls Committed: {stats.FoulsCommitted}</li>
               </ul>
             </div>
           </div>
