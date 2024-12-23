205f8f2ad48d216f9c0096c42a6443d2a8a6787d
Kangdy <canfrog@naver.com>
2024-12-23 15:51:10 +0900

    ✨ [Feat] 스탯 세부 페이지 구현 (4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,6 @@ module.exports = {
         screens: {
           sm: {  max: "480px" },
           md: { min: "481px", max: "1080px" },
-          
           lg: { min: "1081px", max: "1440px" },
         },
         fontFamily: {
